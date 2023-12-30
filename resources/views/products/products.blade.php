cd3fe3ac262f3c99c7da4efd236cbaa07a4630e1
kbasanta21@tbc.edu.np <kbasanta21@tbc.edu.np>
2023-12-30 17:03:36 +0545

    frontend edit

--- resources/views/products/products.blade.php
+++ resources/views/products/products.blade.php
@@ -1,50 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
+
 <body>
-  <h1>Your products</h1>
-  <div>
-    <table border="2">
-      <tr>
-        <th>Id</th>
-        <th>Product Name</th>
-        <th>Quantity</th>
-        <th>Price</th>
-        <th>Description</th>
-        <th>Product Image</th>
-        <th>Edit</th>
-        <th>Delete</th>
-        <th>Product Image</th>
-      </tr>
-      @foreach ($products as $product )
-        <tr>
-          <td>{{ $product->id }}</td>
-          <td>{{ $product->name }}</td>
-          <td>{{ $product->qty }}</td>
-          <td>{{ $product->price }}</td>
-          <td>{{ $product->description }}</td>
-          <td><img src="{{ asset($product->product_image) }}" alt="{{ $product->name }} Image"></td>
-          <td>
-            <a href="{{ route('product.edit',['product'=> $product]) }}">Edit </a>
-          </td>
-          <td>
-            <form action="{{ route('product.delete',['product'=>$product ]) }}" method="POST">
-              @csrf 
-              @method('delete')
-              <input type="submit" value="Delete">
-            </form>
-          </td>
-        </tr>
-      @endforeach
-    </table>
-  </div>
-  <div>
-    <a href="{{ route('product.add') }}">Add product</a>
-  </div>
+    <h1>Your products</h1>
+    <div>
+        <table border="2">
+            <tr>
+                <th>Id</th>
+                <th>Product Name</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Description</th>
+                <th>Product Image</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+            @foreach ($products as $product)
+                <tr>
+                    <td>{{ $product->id }}</td>
+                    <td>{{ $product->name }}</td>
+                    <td>{{ $product->qty }}</td>
+                    <td>{{ $product->price }}</td>
+                    <td>{{ $product->description }}</td>
+                    <td><img src="{{ asset($product->product_image) }}" alt="{{ $product->name }} Image"></td>
+                    <td>
+                        <a href="{{ route('product.edit', ['product' => $product]) }}">Edit </a>
+                    </td>
+                    <td>
+                        <form action="{{ route('product.delete', ['product' => $product]) }}" method="POST">
+                            @csrf
+                            @method('delete')
+                            <input type="submit" value="Delete">
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+    <div>
+        <a href="{{ route('product.add') }}">Add product</a>
+    </div>
 </body>
+
 </html>
