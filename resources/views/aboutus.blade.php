2fdbbd9b9714cc93f51445e5c0cd5ad9a2a5dcc9
kbasanta21@tbc.edu.np <kbasanta21@tbc.edu.np>
2024-01-02 02:06:05 +0545

    route

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -62,7 +62,7 @@
         <nav class="container mx-auto py-4">
             <ul class="flex justify-center space-x-6">
                 <li><a href="/" class="text-gray-700 hover:text-gray-900">Home</a></li>
-                <li><a href="/service" class="text-gray-700 hover:text-gray-900">Services</a></li>
+                <li><a href="{{ route('services.index') }}" class="text-gray-700 hover:text-gray-900">Services</a></li>
                 <li><a href="/about-us" class="text-gray-700 hover:text-gray-900">About Us</a></li>
                 <li><a href="/contacts" class="text-gray-700 hover:text-gray-900">Contacts</a></li>
             </ul>
